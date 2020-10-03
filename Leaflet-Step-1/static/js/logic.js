cea38046af6e5c2ef5d30ce9f0d1f6ec34ebacf7
alissavokes <alissavokes@gmail.com>
2020-10-03 11:24:53 -0400

    updated markers to circles that change color/size/opacity based on magnitude

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -16,9 +16,62 @@ function createFeatures(earthquakeData) {
       "</h3><hr><p>" + new Date(feature.properties.time) + "<hr></p> Magnitude: " + feature.properties.mag);
   }
 
+  //function to increase circle size for magnitudes
+  function magRadius (magnitude) {
+      return magnitude *10;
+  }
+
+  //function to change circle colors based on magnitude
+  function magColor(magnitude) {
+      if (magnitude <= 1) {
+          return "#7cff00"
+        }
+      else if (magnitude <= 2) {
+        return "#c2ff00"
+        }
+        else if (magnitude <= 3) {
+            return "#fff600"
+        }
+        else if (magnitude <= 4) {
+            return "#ffd300"
+        }
+        else if (magnitude <= 5) {
+            return "#ff8c00"
+        }
+        else if (magnitude <= 6) {
+            return "#ff4600"
+        }
+        else {
+            return "#ff0000"
+        }
+    }
+
+    //function to change color opacity based on magnitude
+    function magOpacity(magnitude) {
+        if (magnitude <= 2) {
+            return 0.4
+        }
+        else if (magnitude <= 4) {
+        return 0.6
+        }
+        else if (magnitude <= 6) {
+            return 0.8
+        }
+        else {
+            return 1.0
+        }
+    }
+
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   let earthquakes = L.geoJSON(earthquakeData, {
+      pointToLayer: function(earthquakeData, latlng){
+          return L.circleMarker(latlng, {
+              radius: magRadius(earthquakeData.properties.mag),
+              color: magColor(earthquakeData.properties.mag),
+              fillOpacity: magOpacity(earthquakeData.properties.mag)
+          });
+      },
     onEachFeature: onEachFeature
   });
 
